800dd3aa12d0a62a8554d92263808c9afd4f0429
mahmoud2sofan <nassersofan5@gmail.com>
2025-12-23 23:24:43 +0200

    Update cards.jsx
    edit card

--- The-Levant/src/Components/Cards/cards.jsx
+++ The-Levant/src/Components/Cards/cards.jsx
@@ -1,40 +1,41 @@
 import React from 'react';
 import './Cards.css';
 import countries from './countriesData.jsx';
-
 import { useNavigate } from 'react-router-dom';
 
 function Cards() {
   const navigate = useNavigate();
-  return (
-    <div className="cards-container">
-      {countries.map((country, index) => (
-        <div
-          className="country-card"
-          key={index}
-          onClick={() => navigate(`/explore/${country.name.toLowerCase()}`)}
-          style={{ cursor: 'pointer' }}
-        >
-          <div className="country-image">
-            <img src={country.image} alt={country.name} />
-          </div>
 
-          <div className="country-content">
-            <h3 className="country-name">{country.name}</h3>
-            <p className="country-description">{country.description}</p>
-            <button
-              className="explore-button"
-              onClick={(e) => {
-                e.stopPropagation();
-                navigate(`/explore/${country.name.toLowerCase()}`);
-              }}
-            >
-              Explore
-            </button>
+  return (
+    <section className="cards-section">
+      <div className="cards-container">
+        {countries.map((country, num) => (
+          <div className="country-card" key={num}>
+
+            <div className="country-image">
+              <img src={country.image} alt={country.name} />
+            </div>
+
+            <div className="country-content">
+              <h3 className="country-name">{country.name}</h3>
+              <p className="country-description">
+                {country.description}
+              </p>
+
+              <button
+                className="explore-button"
+                onClick={() =>
+                  navigate(/explore/${country.name.toLowerCase()})
+                }
+              >
+                Explore
+              </button>
+            </div>
+
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </section>
   );
 }
 

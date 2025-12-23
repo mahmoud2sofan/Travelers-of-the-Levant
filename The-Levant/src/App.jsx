173883af4dbde47d146a79979b5c943ca3451695
Mohammad14567 <evilabosaleh@gmail.com>
2025-12-23 18:43:15 +0300

    Update in App.jsx

--- The-Levant/src/App.jsx
+++ The-Levant/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Pages/02-Login/Login';
 import Home from './Pages/03-Home/Home';
+import Attraction from './Pages/05-Attraction/Attraction';
 
 
 function App() {
